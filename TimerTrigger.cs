c677a8b6a998922656635ec941b0397829b8c870
Ayodeji Abodunrin <aboayosam@gmail.com>
2020-05-04 16:16:14 +0100

    Added Twilio sending API

--- TimerTrigger.cs
+++ TimerTrigger.cs
@@ -2,6 +2,8 @@ using System.Globalization;
 using System.Linq;
 using System;
 using System.Collections.Generic;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -12,7 +14,7 @@ namespace DeevCorp.Function
     {
         [FunctionName("TimerTrigger")]
         public static void Run(
-            [TimerTrigger("0 */1 * * * *")]
+            [TimerTrigger("0 */5 * * * *")]
             TimerInfo myTimer,
             [CosmosDB(
                 Constants.COSMOS_DB_DATABASE_NAME,
@@ -40,10 +42,48 @@ namespace DeevCorp.Function
 
             if (usersWithBirthdateToday.Count == 0) return;
 
+            InitializeTwilio();
+
             usersWithBirthdateToday.ForEach(user =>
             {
-                log.LogInformation("Happy Birthday: {0}", (string)user.name.firstName);
+                // Send SMS to recipient
+                if (user.messages != null && user.messages.Count > 0)
+                {
+                    var message = $"{user.messages[user.messages.Count - 1]}";
+
+                    try
+                    {
+                        SendSMS(message, (string)user.phoneNumber, false);
+
+                        log.LogInformation($"Sent message to {user.name.firstName}");
+                    }
+                    catch (Exception e)
+                    {
+                        log.LogError(e.Message);
+                    }
+                }
             });
         }
+
+        public static void SendSMS(string text, string recipient, bool isSending = false)
+        {
+            if (isSending)
+            {
+                MessageResource.Create(
+                   body: text,
+                   from: new Twilio.Types.PhoneNumber(Environment.GetEnvironmentVariable("TwilioNumber")),
+                   to: new Twilio.Types.PhoneNumber(recipient)
+               );
+            }
+        }
+
+        public static void InitializeTwilio()
+        {
+            var twilioAccountSid = Environment.GetEnvironmentVariable("TwilioAccountSid");
+            var twilioAuthToken = Environment.GetEnvironmentVariable("TwilioAuthToken");
+
+            TwilioClient.Init(twilioAccountSid, twilioAuthToken);
+        }
+
     }
 }
